d7d96627ae7834abf05c7b35b515f1beeadf3076
aviaryan <avi.aryan123@gmail.com>
2016-09-10 15:44:25 +0530

    created readability module to get text from article

---
+++ nkm/helpers/parser.py
@@ -0,0 +1,22 @@
+import requests
+from readability import Document
+
+
+def get_page_html(url):
+    """
+    Gets HTML of a page
+    """
+    response = requests.get(url)
+    return response.text
+
+
+def extract_article_info(text):
+    """
+    Gets simplified page from the text
+    Uses readability module
+    """
+    doc = Document(text)
+    return {
+        'title': doc.title(),
+        'content': doc.summary()
+    }
--- nkm/views.py
+++ nkm/views.py
@@ -1,8 +1,10 @@
 from nkm import logger, app
 from nkm.models.user import User
+from helpers.parser import get_page_html, extract_article_info
 
 
 @app.route('/')
 def home():
     logger.info('Hell is this')
-    return 'Hello America'
+    z = get_page_html('http://aviaryan.in/blog/gsoc/docker-compose-starting.html')
+    return extract_article_info(z)['content']
