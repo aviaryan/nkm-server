a840c4a2af9bee0789e12f71fac97a57848da51e
aviaryan <avi.aryan123@gmail.com>
2016-09-11 11:37:32 +0530

    added web homepage

--- nkm/views/public_views.py
+++ nkm/views/public_views.py
@@ -1,14 +1,10 @@
-import json
+# import json
+from flask import render_template
 from nkm import logger, app
-from nkm.helpers.parser import get_page_html, extract_article_info
-from nkm.helpers.feeds import get_feeds
+# from nkm.helpers.parser import get_page_html, extract_article_info
+# from nkm.helpers.feeds import get_feeds
 
 
 @app.route('/')
 def home():
-    logger.info('Hell is this')
-    # z = get_page_html(
-    #     'http://aviaryan.in/blog/gsoc/docker-compose-starting.html'
-    # )
-    # return extract_article_info(z)['content']
-    return json.dumps(get_feeds('Artifical Intelligence'))
+    return render_template('/index.html')
